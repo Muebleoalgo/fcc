cbeaeb31f9bea8c62dce75e347e193b8b0105803
Muebleoalgo <josemartincabrera8@gmail.com>
2024-10-26 22:47:07 -0500

    Segundo commit

---
+++ src/backend/server.js
@@ -0,0 +1,57 @@
+import express from 'express';
+import mercadopago from 'mercadopago';
+import paymentRoutes from './routes/payment.routes.js';
+import morgan from 'morgan';
+require('dotenv').config();
+const accessToken = process.env.ACCESS_TOKEN;
+
+
+// Configura las credenciales de Mercado Pago
+mercadopago.configurations = {
+  access_token: accessToken,
+};
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.use('/api', paymentRoutes); // Usa un prefijo para las rutas del archivo payment.routes.js
+
+app.use(express.static('public'));
+app.use(express.json());
+
+// Ruta para crear la preferencia de pago
+app.post('/create-preference', async (req, res) => {
+  try {
+    const preference = {
+      items: [
+        {
+          title: 'Donación a la Fundación',
+          unit_price: 50, // Precio en soles
+          quantity: 1,
+        },
+      ],
+      back_urls: {
+        success: 'https://tu-sitio.com/success',
+        failure: 'https://tu-sitio.com/failure',
+        pending: 'https://tu-sitio.com/pending',
+      },
+      auto_return: 'approved',
+    };
+
+    console.log('MercadoPago Preferences:', mercadopago.preferences);
+    console.log('Creando preferencia...');
+
+    const response = await mercadopago.preferences.create(preference);
+    console.log('Preferencia creada:', response.body);
+
+    res.json({ id: response.body.id });
+  } catch (error) {
+    console.error('Error en la creación de preferencia:', error);
+    res.status(500).json({ error: `Error en la creación de preferencia: ${error.message}` });
+  }
+});
+
+// Iniciar el servidor
+const PORT = 4242;
+app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
---
+++ src/navigation.ts
@@ -0,0 +1,157 @@
+import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+
+export const headerData = {
+  links: [
+    {
+      text: 'Nosotros',
+      links: [
+        {
+          text: 'Junta Directiva',
+          href: getPermalink('/nosotros/junta_directiva'),
+        },
+        {
+          text: 'Equipo',
+          href: getPermalink('/nosotros/equipo'),
+        },
+        {
+          text: 'Nuestra Historia',
+          href: getPermalink('/nosotros/historia'),
+        },
+      ],
+    },
+    {
+      text: 'Donaciones',
+      links: [
+        {
+          text: 'Cómo donar',
+          href: getPermalink('/donaciones/donaciones'),
+        },
+        {
+          text: 'Beneficiarios',
+          href: getPermalink('/donaciones/beneficiarios'),
+        },
+        {
+          text: 'Dona ahora!',
+          href: getPermalink('/donaciones/dona_ahora'),
+        },
+      ],
+    },
+    {
+      text: 'Voluntarios',
+      links: [
+        {
+          text: 'Únete como voluntario',
+          href: getPermalink('/voluntarios/voluntarios'),
+        },
+        {
+          text: 'Eventos de Voluntariado',
+          href: getPermalink('/voluntarios/eventos'),
+        },
+      ],
+    },
+    {
+      text: 'Proyectos',
+      links: [
+        {
+          text: 'Proyectos en curso',
+          href: getPermalink('/proyectos/proyectos'),
+        },
+        {
+          text: 'Proyectos completados',
+          href: getPermalink('/proyectos/completados'),
+        },
+      ],
+    },
+    {
+      text: 'Contacto',
+      links: [
+        {
+          text: 'Formulario de contacto',
+          href: getPermalink('/contacto'),
+        },
+        {
+          text: 'Preguntas Frecuentes',
+          href: getPermalink('/contacto/preguntas'),
+        },
+      ],
+    },
+    {
+      text: 'Blog',
+      links: [
+        {
+          text: 'Blog List',
+          href: getBlogPermalink(),
+        },
+        {
+          text: 'Artículo de Ejemplo',
+          href: getPermalink('/blog/get-started-website-with-astro-tailwind-css'),
+        },
+      ],
+    },
+  ],
+  // Añadido el botón de donación al headerData para mejor coherencia
+  actions: [
+    {
+      text: 'Dona ahora!',
+      href: getPermalink('/donaciones/dona_ahora'),
+      icon: 'tabler:gift',
+    },
+  ],
+};
+
+export const footerData = {
+  links: [
+    {
+      title: 'NOSOTROS',
+      links: [
+        { text: 'Junta Directiva', href: '/nosotros/junta_directiva' },
+        { text: 'Equipo', href: '/nosotros/equipo' },
+        { text: 'Nuestra Historia', href: '/nosotros/historia' },
+      ],
+    },
+    {
+      title: 'INICIATIVAS ',
+      links: [
+        { text: 'Empresas', href: '/donaciones/empresas' },
+        { text: 'Fondos', href: '/donaciones/fondos' }
+      ],
+    },
+    {
+      title: 'VOLUNTARIADO',
+      links: [
+        { text: 'Únete como voluntario', href: '/voluntarios/unete_voluntario' },
+        { text: 'Eventos de voluntariado', href: '/voluntarios/eventos' },
+      ],
+    },
+    {
+      title: 'PROYECTOS',
+      links: [
+        { text: 'Proyectos en curso', href: '/proyectos/proyectos_actuales' },
+        { text: 'Proyectos completados', href: '/proyectos/proyectos_completados' },
+      ],
+    },
+    {
+      title: 'CONTÁCTANOS',
+      links: [
+        { text: 'X', icon: 'tabler:brand-x', href: '#' },
+        { text: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
+        { text: 'Facebook', icon: 'tabler:brand-facebook', href: '#' }
+      ],
+    },
+  ],
+  secondaryLinks: [
+    { text: 'Términos y condiciones', href: getPermalink('/terms') },
+    { text: 'Políticas de privacidad', href: getPermalink('/privacy') },
+  ],
+  socialLinks: [
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: '#' },
+  ],
+  footNote: `
+    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="Quipu logo" loading="lazy"></img>
+    Desarrollado por <a class="text-blue-600 underline dark:text-muted" href="https://Quipu.com/"> Quipu</a> · Todos los derechos reservados.
+  `,
+};

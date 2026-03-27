50e067accc5b1b5a60bb5fcf5648d342d3a00830
Muebleoalgo <josemartincabrera8@gmail.com>
2026-03-27 17:06:59 -0500

    Logo footer Quipu

--- src/navigation.ts
+++ src/navigation.ts
@@ -151,7 +151,7 @@ export const footerData = {
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: '#' },
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="Quipu logo" loading="lazy"></img>
+    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="/images/Logo-Quipu-png-Photoroom.webp" alt="Quipu logo" loading="lazy"></img>
     Desarrollado por <a class="text-blue-600 underline dark:text-muted" href="https://Quipu.com/"> Quipu</a> · Todos los derechos reservados.
   `,
 };
